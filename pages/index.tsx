cbdc94ebd817accc8ef54866044c46c970470fe4
propertiess <erofeev2001@yandex.ru>
2023-01-11 13:07:58 +0300

    chore: remove useless components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next';
-import { HomeScreen } from '@/screens/HomeScreen';
+import { Hero } from '@/components/home/Hero';
+import { Layout } from '@/layout/Layout';
 
 const Home: NextPage = () => {
-  return <HomeScreen />;
+  return (
+    <Layout
+      title='Онлайн конструктор резюме'
+      description='Сделать резюме быстро и легко по готовым шаблонам. Только 5% соискателей имеют качественное резюме - будь среди них и быстро найти хорошую работу.'
+    >
+      <Hero />
+    </Layout>
+  );
 };
 
 export default Home;
--- pages/rezumator.tsx
+++ pages/rezumator.tsx
@@ -1,8 +1,59 @@
 import { NextPage } from 'next';
-import { RezumatorScreen } from '@/screens/RezumatorScreen';
+import { Button } from '@/components/common/ui/Button';
+import { About } from '@/components/rezumator/about';
+import { Education } from '@/components/rezumator/education';
+import { Experience } from '@/components/rezumator/experience';
+import { Optional } from '@/components/rezumator/optional';
+import { Personal } from '@/components/rezumator/personal';
+import { useRezumatorForm } from '@/hooks/useRezumatorForm';
+import { Layout } from '@/layout/Layout';
 
 const Rezumator: NextPage = () => {
-  return <RezumatorScreen />;
+  const {
+    register,
+    control,
+    errors,
+    onSubmit,
+    isSubmitting,
+    isSubmitSuccessful
+  } = useRezumatorForm();
+
+  return (
+    <Layout title='Составить резюме' description='Составить резюме'>
+      <form onSubmit={onSubmit}>
+        <About register={register} errors={errors.rezumator?.aboutInfo} />
+        <Personal register={register} errors={errors.rezumator?.personalInfo} />
+        <Education
+          register={register}
+          errors={errors.rezumator?.educationInfo}
+          control={control}
+        />
+        <Experience
+          register={register}
+          errors={errors.rezumator?.experienceInfo}
+          control={control}
+        />
+        <Optional
+          register={register}
+          errors={errors.rezumator?.optionalInfo}
+          control={control}
+        />
+
+        {!isSubmitSuccessful ? (
+          <Button
+            className='flex mt-5 ml-auto'
+            disabled={isSubmitting}
+            type='submit'
+            loader={isSubmitting}
+          >
+            Предпросмотр
+          </Button>
+        ) : (
+          <span className='flex justify-end mt-5'>Переходим к шаблону...</span>
+        )}
+      </form>
+    </Layout>
+  );
 };
 
 export default Rezumator;
