e7ea3be21ad94b02c3de66cdce174a90e31ad42f
propertiess <erofeev2001@yandex.ru>
2023-01-15 17:38:29 +0300

    refactor: upd prettier deps

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,24 +1,3 @@
 module.exports = {
-  tabWidth: 2,
-  arrowParens: 'avoid',
-  endOfLine: 'crlf',
-  semi: true,
-  trailingComma: 'none',
-  singleQuote: true,
-  jsxSingleQuote: true,
-  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
-  importOrderSeparation: true,
-  importOrder: [
-    '^(react|next(.*))$',
-    '<THIRD_PARTY_MODULES>',
-    '^@/components(.*)$',
-    '^@/(hooks|store/hooks(.*))$',
-    '^@/store(.*)$',
-    '^@/context(.*)$',
-    '^@/services(.*)$',
-    '^@/types(.*)$',
-    '^@/(.*)$',
-    '^(.*)(sass|css|scss)$'
-  ],
-  importOrderSortSpecifiers: true
+  ...require('@propertiess/prettier-config')
 };
