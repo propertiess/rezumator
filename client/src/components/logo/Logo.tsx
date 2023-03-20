ca7dc3094c7f9b29d0d3931dfd6f1b23f3c96f48
propertiess <erofeev2001@yandex.ru>
2023-03-21 00:23:53 +0300

    style: remove image logo wrapper

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,16 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import LogoAvatar from '../../../public/logo.svg';
+import LogoAvatar from '@/../public/logo.svg';
 
 import styles from './Logo.module.css';
 
 export const Logo = () => {
   return (
     <Link className={styles.wrapper} href='/'>
-      <span className='relative block h-[30px] w-[30px]'>
-        <Image className='image' src={LogoAvatar} alt='logo' sizes='100vw' />
-      </span>
+      <Image width={30} height={28} src={LogoAvatar} alt='Logo' />
       <h2 className={styles.text}>Rezumator</h2>
     </Link>
   );
